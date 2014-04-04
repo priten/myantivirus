0b07f2bb91f0822c115cc94a0ec7dc2ce2db9d38
priten <priten_patel@hotmail.co.uk>
2014-04-04 15:05:54 +0100

    initial upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.prgsbar = new System.Windows.Forms.ProgressBar();
+            this.lblfolderpath = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstbox = new System.Windows.Forms.ListBox();
+            this.lblvirus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 89);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(140, 44);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse Folder";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(158, 89);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(140, 44);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Start Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // prgsbar
+            // 
+            this.prgsbar.Location = new System.Drawing.Point(12, 192);
+            this.prgsbar.Name = "prgsbar";
+            this.prgsbar.Size = new System.Drawing.Size(678, 35);
+            this.prgsbar.TabIndex = 2;
+            // 
+            // lblfolderpath
+            // 
+            this.lblfolderpath.AutoSize = true;
+            this.lblfolderpath.Location = new System.Drawing.Point(12, 150);
+            this.lblfolderpath.Name = "lblfolderpath";
+            this.lblfolderpath.Size = new System.Drawing.Size(57, 13);
+            this.lblfolderpath.TabIndex = 3;
+            this.lblfolderpath.Text = "folder path";
+            // 
+            // lstbox
+            // 
+            this.lstbox.FormattingEnabled = true;
+            this.lstbox.Location = new System.Drawing.Point(376, 20);
+            this.lstbox.Name = "lstbox";
+            this.lstbox.Size = new System.Drawing.Size(313, 147);
+            this.lstbox.TabIndex = 4;
+            // 
+            // lblvirus
+            // 
+            this.lblvirus.AutoSize = true;
+            this.lblvirus.Location = new System.Drawing.Point(14, 174);
+            this.lblvirus.Name = "lblvirus";
+            this.lblvirus.Size = new System.Drawing.Size(79, 13);
+            this.lblvirus.TabIndex = 5;
+            this.lblvirus.Text = "viruses found : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 239);
+            this.Controls.Add(this.lblvirus);
+            this.Controls.Add(this.lstbox);
+            this.Controls.Add(this.lblfolderpath);
+            this.Controls.Add(this.prgsbar);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ProgressBar prgsbar;
+        private System.Windows.Forms.Label lblfolderpath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lstbox;
+        private System.Windows.Forms.Label lblvirus;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+
+public class streamreader { }
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int virus;
+        //here i have defined the variable "virus" as an integer 
+        string[] viruslist = new string[] { "30abd218790605c5882fcacf2fac1861", "f49eb02f70e6c17f72551b5c287f9889", "9e1c2f8106b932896bd4970520a884e6" };
+       //the viruslist hase been delcared as a string and within it are md5 hashes which should be picked up by the program.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            lblfolderpath.Text = folderBrowserDialog1.SelectedPath;
+            //this section of code allows the user to click the button and select file path
+            //once it is selected it will then be shown in the lable below
+            virus = 0;
+            lblvirus.Text = virus.ToString();
+            prgsbar.Value = (0);
+            //sets the values of the progress bar to "0" 
+            lstbox.Items.Clear();
+            //this section indicates that there is no information in the list box
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> search = Directory.GetFiles(@folderBrowserDialog1.SelectedPath, "*.*").ToList();
+            //this line of code basically searched the directory within the system and goes through the selected path and goes through every character
+            prgsbar.Maximum = search.Count;
+            //the progress bar has been set to maximum and adds the count within the bar
+
+            foreach (string item in search)
+                //defining each strin item that have been put into the search.
+            {
+                MessageBox.Show(hashFile(item));
+                //the message box opens showing the hash of the folder path
+            }
+        }
+
+        private string hashFile(string loc)
+            //function that returns the file as a md5 hash 
+        {
+            using (var md5 = MD5.Create())
+            //get instance of md5
+            {
+                using (var stream = File.OpenRead(loc))
+                //open file to md5
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-","").ToLower();
+                    //returns the md5
+                    //http://stackoverflow.com/questions/10520048/calculate-md5-checksum-for-a-file
+                    //https://www.youtube.com/watch?v=jjN2VNY5w8g
+                }
+            }
+        }
+    }
+}
